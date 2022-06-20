1e3811cdb957d1ab17b1332b716845bb9b0ba598
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2022-06-20 17:42:57 +0200

    act "la menace glicth saw"

---
+++ core_base.php
@@ -0,0 +1,27 @@
+<?php require 'lib.php'; ?>
+
+<form action="break_core_verif.php" method="post">
+    <label for="core_1"></label>
+    <input type="text" name="core_1">
+    <input type="submit" value="→">
+</form>
+
+<form action="break_core_verif.php" method="post">
+    <label for="core_2"></label>
+    <input type="text" name="core_2">
+    <input type="submit" value="→">
+</form>
+
+<form action="break_core_verif.php" method="post">
+    <label for="core_3"></label>
+    <input type="text" name="core_3">
+    <input type="submit" value="→">
+</form>
+
+<form action="break_core_verif.php" method="post">
+    <label for="core_4"></label>
+    <input type="text" name="core_4">
+    <input type="submit" value="→">
+</form>
+
+
