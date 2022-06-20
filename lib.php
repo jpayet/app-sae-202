de7cf026aa99f9a9d312888db5a9bb7da82d6a0c
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2022-06-20 14:13:50 +0200

    profile updating function and page

--- lib.php
+++ lib.php
@@ -37,10 +37,42 @@
                     echo '<img src="img/uploads/'.$column['profile_pict'].'" alt="profile picture" />';
                     echo '<p>Bienvenue, ' . $_SESSION['user_name'] . '</p>' . "\n";
                     echo '<p>Membre de ' . $column['name'] . '</p>' . "\n";
-                    echo '<a href="profile.php?id='.$_SESSION['user_id'].'">modifier le profil</a> | <a href="logout.php">Déconnexion</a>';
+                    echo '<a href="profile.php">modifier le profil</a> | <a href="logout.php">Déconnexion</a>';
             }
         } else {
             echo '<p> Bienvenue sur Dual Glitch, connectez vous pour accéder à votre QG </p>';
+        }
+    }
+
+    function getUser($db, $idUser){
+        $req=$db->prepare('SELECT * FROM user WHERE user_id = :idUser');
+        try {
+            $req->execute(array(
+                ':idUser' => $idUser
+            ));
+        } catch (PDOException $e){
+            echo '<p>Erreur : ' . $e->getMessage() . '</p>';
+            die();
+        }
+        return $req->fetch();
+    }
+
+    function update_user_profile($db, $id, $newPicture){
+        $req=$db->prepare('UPDATE user SET profile_pict = :picture WHERE user_id = :id');
+        try {
+            $req->execute(array(
+                ':picture' => $newPicture,
+                ':id' => $id
+            ));
+        } catch (PDOException $e){
+            echo '<p>Erreur : ' . $e->getMessage() . '</p>';
+            die();
+        }
+        if ($req->rowCount() == 1){
+            header('location: index.php');
+        } else {
+            echo '<p> Oups... Une erreur est survenue </p>';
+            die();
         }
     }
 
