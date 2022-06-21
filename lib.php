03fd978a24aca0c7085d90c0c9aa22c44bc10832
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2022-06-21 10:14:34 +0200

    repair bug

--- lib.php
+++ lib.php
@@ -51,7 +51,7 @@
                     echo '</div>';
             }
         } else {
-            echo '<p> Bienvenue sur Dual Glitch, <br/> connectez vous pour accéder à votre QG </p>';
+            echo '<p> Bienvenue sur Dual Glitch, <br/> connectez vous pour accéder à votre QG </p><a href="login.php">Connexion</a>';
         }
     }
 
